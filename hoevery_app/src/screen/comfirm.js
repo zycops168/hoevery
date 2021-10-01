5be6a9ae57f3bd79020726f11400284c1b1c9a0b
CHARMUAR <laewtaebenz@gmail.com>
2021-10-01 22:06:26 +0700

    king request

---
+++ hoevery_app/src/screen/comfirm.js
@@ -0,0 +1,8 @@
+
+var name = "benz"
+if(name == "benz"){
+console.log("Name :" + name)
+}
+else if ( name.length  > 3){
+    console.log("name  is : " + name.length)
+}
--- hoevery_app/src/screen/mainPage.js
+++ hoevery_app/src/screen/mainPage.js
@@ -238,3 +238,6 @@ export default mainPage;
 //      </TouchableOpacity>   
 //     </View> 
 //  </View>
+
+
+
